8b18df79eac48ebe78848cd831d52e3f67a6c765
jesus <jesuscox@gmail.com>
2021-02-16 21:10:28 -0500

    First commit

---
+++ src/test/java/qaclickacademy/Mavenjava1/RESTAPITest.java
@@ -0,0 +1,22 @@
+package qaclickacademy.Mavenjava1;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+	
+	@Test
+	public void postJita()
+	{
+		
+		System.out.println("postJita");
+		
+	}
+
+	@Test
+	public void deleteTwitter()
+	{
+		
+		System.out.println("deleteTwitter");
+	}
+
+}
---
+++ src/test/java/qaclickacademy/Mavenjava1/SeleniumTest.java
@@ -0,0 +1,27 @@
+package qaclickacademy.Mavenjava1;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	
+	//testing xml files from maven
+	//how to execite all test casesfrom test folder
+	
+	@Test
+	public void BrowserAutomation()
+	{
+		
+		System.out.println("BrowserAutomation");
+		
+	}
+	
+	@Test
+	public void elementsUI()
+	{
+		
+		System.out.println("elementsUI");
+	}
+
+
+}
